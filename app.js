61c52ee808f946b04997a640515a9c4ba54faae6
zhengjianliu <comezheng@gmail.com>
2021-01-17 20:18:03 -0500

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>Page not Found!</h1>")
+})
+ 
+app.listen(3000);
